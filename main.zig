b2283352bc58f2a7fec055dade75317a70db10d6
Jacob Strieb <jstrieb@users.noreply.github.com>
2024-06-22 12:08:56 -0400

    Finalize (slow) first-pass brute forcer

--- main.zig
+++ main.zig
@@ -1,5 +1,9 @@
 const std = @import("std");
 
+const base = "jstrieb/";
+const size = 64;
+var most_zeroes: usize = 0;
+
 fn count_zeroes(a: []u8) usize {
     var result: usize = 0;
     for (a) |b| {
@@ -15,22 +19,29 @@ fn count_zeroes(a: []u8) usize {
     return result;
 }
 
+fn try_all(in: []u8, out: *[32]u8, length: usize) void {
+    if (length <= 0) {
+        std.crypto.hash.sha2.Sha256.hash(in, out, .{});
+        const zeroes = count_zeroes(out);
+        if (zeroes > most_zeroes) {
+            std.debug.print("{d} {s} {s}\n", .{ zeroes, in, std.fmt.bytesToHex(out, std.fmt.Case.lower) });
+            most_zeroes = zeroes;
+        }
+        return;
+    }
+    for ('a'..'z' + 1) |c| {
+        in[base.len + length - 1] = @intCast(c);
+        try_all(in, out, length - 1);
+    }
+}
+
 pub fn main() !void {
-    const base = "jstrieb/a";
-    var in: [64]u8 = .{0} ** 64;
+    var in: [size]u8 = .{0} ** size;
     @memcpy(in[0..base.len], base);
     var out: [32]u8 = undefined;
-    var length: usize = 2;
-    while (true) {
-        for (1..length) |i| {
-            for ('a'..'z' + 1) |c| {
-                in[base.len + i] = @intCast(c);
-                std.crypto.hash.sha2.Sha256.hash(in[0 .. base.len + length], &out, .{});
-                std.debug.print("{s} {s}\n", .{ in[0 .. base.len + length], std.fmt.bytesToHex(out, std.fmt.Case.lower) });
-                std.debug.print("{d}\n", .{count_zeroes(&out)});
-            }
-        }
+    var length: usize = 1;
+    while (length < size - base.len) {
+        try_all(in[0 .. base.len + length], &out, length);
         length += 1;
-        break;
     }
 }
